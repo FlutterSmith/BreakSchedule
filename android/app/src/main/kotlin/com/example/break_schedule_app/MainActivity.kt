817711abc89d13a8970dfdd85e64bb6a8684c7ef
Ahmed_hamdy <118023886+SiNfuuL@users.noreply.github.com>
2025-02-04 01:28:41 +0200

    Refactor Android build configuration; update package name and dependencies

--- android/app/src/main/kotlin/com/example/break_schedule_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/break_schedule_app/MainActivity.kt
@@ -1,5 +1,5 @@
-package com.example.break_schedule_app
+package com.example.break_reminder_app
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity : FlutterActivity() // Kotlin syntax
